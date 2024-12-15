12ac1399670bc0d7c11f9398bac28c5c606a9e91
Aboubakr06 <aboubakerelbouzidi@gmail.com>
2024-12-15 04:06:35 +0100

    using fetch in js and refactor html

--- script.js
+++ script.js
@@ -1,61 +1,73 @@
-let periods = ['.daily', '.weekly', '.monthly'];
+const container = document.getElementById("container");
+const timeEl = document.getElementById("timeEl");
 
-let categories = [
-  { current: '.work-current', last: '.work-last' },
-  { current: '.play-current', last: '.play-last' },
-  { current: '.study-current', last: '.study-last' },
-  { current: '.exercise-current', last: '.exercise-last' },
-  { current: '.social-current', last: '.social-last' },
-  { current: '.care-current', last: '.care-last' }];
+const appendItem = (item) => {
+  const domElement = `<div style="background-color: var(--primary-${item.dataTitle}); background-image: url('/images/icon-${item.dataTitle}.svg');" class="rounded-2xl pt-12 bg-no-repeat bg-[right_1rem_top]">
+  <div class="grid gap-4 rounded-2xl p-8 bg-[var(--neutral-blue)] cursor-pointer hover:bg-[var(--neutral-blue-hover)]">
+    <div class="flex items-center justify-between">
+      <h2 class="font-medium">${item.title}</h2>
+      <img src="images/icon-ellipsis.svg" alt="" aria-hidden="true">
+    </div>
+    <div class="flex items-center gap-4 justify-between lg:grid">
 
-let currentPeriod;
-let values = {
-  daily: [
-    { current: 5, last: 'Day - 7' },
-    { current: 1, last: 'Day - 2' },
-    { current: 0, last: 'Day - 1' },
-    { current: 1, last: 'Day - 1' },
-    { current: 1, last: 'Day - 3' },
-    { current: 0, last: 'Day - 1' }
-  ],
-  weekly: [
-    { current: 32, last: 'Week - 36' },
-    { current: 10, last: 'Week - 8' },
-    { current: 4, last: 'Week - 7' },
-    { current: 4, last: 'Week - 5' },
-    { current: 5, last: 'Week - 10' },
-    { current: 2, last: 'Week - 2' }
-  ],
-  monthly: [
-    { current: 103, last: 'Month - 128' },
-    { current: 23, last: 'Month - 29' },
-    { current: 13, last: 'Month - 19' },
-    { current: 11, last: 'Month - 18' },
-    { current: 21, last: 'Month - 23' },
-    { current: 7, last: 'Month - 11' }
-  ]
-};
+      <p class="font-light text-xl lg:text-6xl"><span>${item.timeframe.current}</span>hrs</p>
+      <p class="text-[var(--neutral-light-violet)] lg:text-base">Last <span>${item.timeframe.previous}</span>hrs</p>
 
-periods.forEach(function (period) {
-  document.querySelector(period).addEventListener('click', function () {
-    currentPeriod = period.substr(1);
+    </div>
+  </div>
+</div>`;
 
-    periods.forEach(function (p) {
-      document.querySelector(p).classList.remove('text-white');
-    });
-    document.querySelector(period).classList.add('text-white');
+    container.innerHTML += domElement;
+}
 
-    categories.forEach(function (category, index) {
-      document.querySelector(category.current).innerHTML = values[currentPeriod][index].current;
-      document.querySelector(category.last).innerHTML = values[currentPeriod][index].last;
-    });
-  });
+const populate = (data) => {
+  container.innerHTML = "";
+  data.map(item => {
+    appendItem(item);
+  })
+}
+
+const changePeriod = (idElements, data) => {
+  const periodEls = document.querySelectorAll(idElements);
+  periodEls.forEach(periodEl => {
+    periodEl.addEventListener('click', () =>{
+      periodEls.forEach(periodEl => {periodEl.classList.remove('text-white');});
+      periodEl.classList.add('text-white');
+      const timePeriod = periodEl.textContent;
+      console.log(timePeriod);
+      populate(filterData(data, timePeriod.toLowerCase()));
+    })
+  });  
+}
+
+const timesPeriod = (keys) => {
+  for (let keyEl of keys){
+    const domElement = `<a class="cursor-pointer hover:text-white" id="period">${keyEl}</a>`
+    timeEl.innerHTML += domElement;
+  }
+
+  const periodEls = document.querySelectorAll("#period");
+  periodEls[1].classList.add("text-white");
+}
+
+const filterData = (data, timePeriod) =>{
+  return data.map(item => {
+    let periodEl = item.timeframes[timePeriod];
+    return {...item, timeframe: periodEl};
+  })
+}
+
+fetch('./data.json').then((response) => {
+  if(!response.ok) return console.log('Oops! Something went wrong.');
+
+  return response.json();
+}).then((data) => {
+  // handle the data
+
+  timesPeriod(Object.keys(data[0].timeframes));
+
+  populate(filterData(data, "weekly"));
+
+  changePeriod("#period", data);
 });
 
-currentPeriod = 'weekly';
-document.querySelector('.weekly').classList.add('text-white');
-
-categories.forEach(function(category, index) {
-  document.querySelector(category.current).innerHTML = values[currentPeriod][index].current;
-  document.querySelector(category.last).innerHTML = values[currentPeriod][index].last;
-});
